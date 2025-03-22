5b8cb90f953aee58c373bfd7172a7a30f56bb755
Gabriel Guerra Rosales <97709500+GabrielGuerra06@users.noreply.github.com>
2025-03-22 11:53:14 -0600

    starlion proyect and update excercise

--- projects/brewery/js/main.js
+++ projects/brewery/js/main.js
@@ -1,6 +1,6 @@
-var margin = {top: 10, right: 10, bottom: 100, left:100};
-var width = 600 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
+var margin = {top: 10, right: 10, bottom: 100, left:200};
+var width = 800 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
 d3.json("data/revenues.json").then((data)=> {
 
 	data.forEach((d)=>{
@@ -11,8 +11,9 @@ d3.json("data/revenues.json").then((data)=> {
 	console.log(data);
 
 	var svg = d3.select("#chart-area").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom);
+	.attr("width", width + margin.left + margin.right + 100)
+	.attr("height", height + margin.top + margin.bottom)
+
 
     var g = svg.append("g")
 	.attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
@@ -24,7 +25,7 @@ d3.json("data/revenues.json").then((data)=> {
 	.paddingOuter(0.3);
 
 	var y = d3.scaleLinear()
-	.domain([0,828])
+	.domain([0,50000])
 	.range([height,0]);
 
 
@@ -36,6 +37,8 @@ d3.json("data/revenues.json").then((data)=> {
 	.attr("y", (d) => { return y(d.revenue); })
 	.attr("width", x.bandwidth())
 	.attr("height", (d)=>{ return height - y(d.revenue); })
+	.attr("fill", "yellow");
+
 
 	var bottomAxis = d3.axisBottom(x);
 	g.append("g")
@@ -43,35 +46,42 @@ d3.json("data/revenues.json").then((data)=> {
 	.attr("transform", "translate(0, " + height + ")")
 	.call(bottomAxis)
 	.selectAll("text")
-	.attr("x", -5)
+	.attr("x", 15)
 	.attr("y", 10)
 	.attr("text-anchor", "end")
-	.attr("transform", "rotate(-40)");
+	.attr("font-size", "15px")
+	.attr("fill", "white");
 
 	var leftAxis = d3.axisLeft(y)
-	.ticks(5)
-	.tickFormat((d) => d + "m");
+	.tickFormat((d) => "$ " + d + "k");
 g.append("g")
 	.attr("class", "left axis")
-	.call(leftAxis);
+	.call(leftAxis)
+	.attr("font-size", "15px")
+	.attr("fill", "white");
+
+g.selectAll(".left.axis .tick text")
+	.attr("fill", "white");
 
 	g.append("text")
 	.attr("class", "x axis-label")
 	.attr("x", width / 2)
-	.attr("y", height + 100)
-	.attr("font-size", "20px")
+	.attr("y", height + 50)
+	.attr("font-size", "30px")
 	.attr("text-anchor", "middle")
-	.text("The world's tallest buildings");
+	.attr("fill", "white")
+	.text("Month");
 
 
 	g.append("text")
 	.attr("class", "y axis-label")
 	.attr("x", -(height / 2))
-	.attr("y", -60)
-	.attr("font-size", "20px")
+	.attr("y", -100)
+	.attr("font-size", "30px")
 	.attr("text-anchor", "middle")
 	.attr("transform", "rotate(-90)")
-	.text("Height (m)");
+	.attr("fill", "white")
+	.text("Revenue (dlls.)");
 
 });
 
---
+++ simulating_update_events/js/main.js
@@ -0,0 +1,111 @@
+var margin = {top: 10, right: 10, bottom: 100, left:200};
+var width = 800 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
+var flag = true;
+
+var svg = d3.select("#chart-area")
+
+.append("svg")
+
+	.attr("width", width + margin.left + margin.right)
+
+	.attr("height", height + margin.top + margin.bottom)
+
+	var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+var x = d3.scaleBand().range([0, width]).padding(0.2);
+var y = d3.scaleLinear().range([height, 0]);
+
+var xAxisGroup = g.append("g")
+    .attr("class", "bottom axis")
+    .attr("transform", "translate(0, " + height + ")");
+
+var yAxisGroup = g.append("g")
+    .attr("class", "left axis");
+
+var yLabel = g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", -(height / 2))
+    .attr("y", -100)
+    .attr("font-size", "30px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .attr("fill", "white")
+    .text("Revenue");
+
+g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 50)
+    .attr("font-size", "30px")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text("Month");
+
+
+d3.json("data/revenues.json").then((data)=> {
+
+	data.forEach((d)=>{
+
+		d.revenue = +d.revenue;
+		d.profit = +d.profit;
+	});
+
+	d3.interval(() => {
+        update(data);
+        flag = !flag;
+    }, 1000);
+
+    update(data);
+}).catch((error) => {
+    console.log(error);
+});
+
+function update(data) {
+    var value = flag ? "revenue" : "profit";
+
+	x.domain(data.map((d) => { return d.month; }));
+    y.domain([0, d3.max(data, (d) => { return d[value]; })]);
+
+	var bottomAxis = d3.axisBottom(x);
+    var leftAxis = d3.axisLeft(y).tickFormat((d) => "$ " + d + "k");
+
+	xAxisGroup.call(bottomAxis)
+	.selectAll("text")
+	.attr("x", 15)
+	.attr("y", 10)
+	.attr("text-anchor", "end")
+	.attr("font-size", "15px")
+	.attr("fill", "white");
+
+yAxisGroup.call(leftAxis)
+	.selectAll("text")
+	.attr("font-size", "15px")
+	.attr("fill", "white");
+
+	var rects = g.selectAll("rect").data(data);
+    rects.exit().remove();
+
+
+    rects.attr("x", (d) => { return x(d.month); })
+        .attr("y", (d) => { return y(d[value]); })
+        .attr("width", x.bandwidth())
+        .attr("height", (d) => { return height - y(d[value]); })
+        .attr("fill", "yellow");
+
+	rects.enter().append("rect")
+	.attr("x", (d) => { return x(d.month); })
+	.attr("y", (d) => { return y(d[value]); })
+	.attr("width", x.bandwidth())
+	.attr("height", (d) => { return height - y(d[value]); })
+	.attr("fill", "yellow");
+
+    var label = flag ? "Revenue" : "Profit";
+    yLabel.text(label);
+}
+
+
+
+
+	
